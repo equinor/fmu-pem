1c255c57403fe0c548e8493bc76694cc26974a9c
Anders Fredrik Kiær <31612826+anders-kiaer@users.noreply.github.com>
2025-09-01 11:23:58 +0000

    lint

--- src/fmu/pem/pem_utilities/pem_config_validation.py
+++ src/fmu/pem/pem_utilities/pem_config_validation.py
@@ -114,9 +114,9 @@ class RockMatrixProperties(BaseModel):
                 "density": 2490.0,
             },
         },
-        description="Define minerals relevant for the field. Default values are set for "
-        "`shale`, `quartz`, `calcite`, `dolomite` and `stevensite` (you can not delete "
-        "these minerals, but you can override their default values and/or "
+        description="Define minerals relevant for the field. Default values are set "
+        "for `shale`, `quartz`, `calcite`, `dolomite` and `stevensite` (you can not "
+        "delete these minerals, but you can override their default values and/or "
         "ignore their definition).",
     )
     volume_fractions: NTGFraction | FractionFiles = Field(
