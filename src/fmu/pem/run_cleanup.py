256a87855ed029f75f9c17ea3ba06d64baed2e67
hflesche <hfle@equinor.com>
2026-06-29 14:04:10 +0200

    feat: add cli entry point

---
+++ src/fmu/pem/run_cleanup.py
@@ -0,0 +1,29 @@
+import os
+import sys
+from pathlib import Path
+
+from fmu.pem.__main__ import _resolve_fmu_rootpath
+from fmu.pem.pem_utilities import restore_dir
+from fmu.pem.pem_utilities.argument_parser import parse_cleanup
+from fmu.pem.pem_utilities.cleanup import cleanup_pem_results
+
+
+def run_pem_cleanup(args_list=None):
+    if args_list is None:
+        args_list = sys.argv[1:]
+    args = parse_cleanup(args_list)
+
+    run_folder = _resolve_fmu_rootpath(config_dir=args.grid_dir.absolute())
+
+    with restore_dir(run_folder):
+        cleanup_pem_results(
+            directory=args.grid_dir,
+            remove_categories=args.save_type_list,
+            is_ensemble=args.is_ensemble,
+            prefix=args.prefix,
+            extension=args.extension,
+        )
+
+
+if __name__ == "__main__":
+    run_pem_cleanup()
