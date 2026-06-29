a15412852b4d6ffaa7b5d2ce63857add606a21b2
hflesche <hfle@equinor.com>
2026-06-29 14:04:10 +0200

    feat: add cli entry point

--- src/fmu/pem/__init__.py
+++ src/fmu/pem/__init__.py
@@ -1,7 +1,9 @@
 from .__main__ import main as pem
+from .run_cleanup import run_pem_cleanup as pem_cleanup
 from .run_pem import pem_fcn
 
 __all__ = [
     "pem_fcn",
     "pem",
+    "pem_cleanup",
 ]
--- src/fmu/pem/pem_utilities/argument_parser.py
+++ src/fmu/pem/pem_utilities/argument_parser.py
@@ -1,6 +1,8 @@
 import argparse
 from pathlib import Path
 
+from fmu.pem.pem_utilities.enum_defs import SaveTypes
+
 
 def _str2bool(value: object) -> bool:
     if isinstance(value, bool):
@@ -19,7 +21,7 @@ def parse_arguments(
     arguments: list[str],
 ) -> argparse.Namespace:
     """
-    Uses argparse to parse arguments as expected from command line invocation
+    Uses argparse to parse arguments as expected from command line invocation for pem
     """
     parser = argparse.ArgumentParser(__file__)
     parser.add_argument(
@@ -51,7 +53,6 @@ def parse_arguments(
         default=False,
         help="Select verbose or minimal output",
     )
-
     # Split config and global path into directory and file name, as this is
     # required in the PEM
     args = parser.parse_args(arguments)
@@ -61,3 +62,54 @@ def parse_arguments(
     args.global_file = Path(args.global_file.name)
 
     return args
+
+
+def parse_cleanup(
+    arguments: list[str],
+) -> argparse.Namespace:
+    """
+    Uses argparse to parse arguments as expected from command line invocation for
+    pem_cleanup
+    """
+    parser = argparse.ArgumentParser(__file__)
+    parser.add_argument(
+        "-g",
+        "--grid_dir",
+        type=Path,
+        required=True,
+        help="Path name of the directory with grid files from PEM run",
+    )
+    parser.add_argument(
+        "-s",
+        "--save_type_list",
+        nargs="+",
+        type=str,
+        choices=[t.value for t in SaveTypes],
+        required=True,
+        help="List of save batches: 'all', 'intermediate', 'difference', 'elastic'",
+    )
+    parser.add_argument(
+        "-i",
+        "--is_ensemble",
+        type=_str2bool,
+        required=False,
+        default=False,
+        help="Delete all grids in an ensemble run, default=False",
+    )
+    parser.add_argument(
+        "-p",
+        "--prefix",
+        type=str,
+        default="simgrid",
+        required=False,
+        help="Prefix in grid file names, default='simgrid'",
+    )
+    parser.add_argument(
+        "-e",
+        "--extension",
+        type=str,
+        default=".roff",
+        required=False,
+        help="Grid file name extension, default='.roff'",
+    )
+    return parser.parse_args(arguments)
